f5bb6b6bed1c7a17634262b65a9432a8414f98e7
DisRachik <radchenkoig@gmail.com>
2023-01-07 23:25:45 +0200

    add script for scale progress

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener("click", () => {
 closeElem.addEventListener("click", () => {
 	menu.classList.remove("active");
 });
+
+const percents = document.querySelectorAll(".skills__percent"),
+	lines = document.querySelectorAll(".skills__scale-progress");
+
+percents.forEach((item, i) => {
+	lines[i].style.width = item.innerHTML;
+});
